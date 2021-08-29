7e786d7bf3c2a4177a6313082a3811a10458674b
AgustinaPaz <pazflorenciaagustina@gmail.com>
2021-08-29 20:33:56 -0300

    clase producto con sus atributos y metodos

--- src/producto/Producto.java
+++ src/producto/Producto.java
@@ -1,5 +1,18 @@
 package producto;
 
 public abstract class Producto {
-
+	
+	private String nombre;
+	private TipoDeAtraccion tipoAtraccion;
+	private double duracion;
+	private double costo;
+	
+	protected double getCosto() {
+		return this.costo;
+	}
+	
+	protected double getDuracion() {
+		return this.duracion;
+	}
+	
 }
