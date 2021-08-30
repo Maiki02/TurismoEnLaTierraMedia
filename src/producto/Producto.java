954d18e74f4a56835d58d497de1a9bc6aab132cc
AgustinaPaz <pazflorenciaagustina@gmail.com>
2021-08-30 13:21:05 -0300

    constructor de la clase producto

--- src/producto/Producto.java
+++ src/producto/Producto.java
@@ -7,6 +7,13 @@ public abstract class Producto {
 	private double duracion;
 	private double costo;
 	
+	public Producto(String nombre, TipoDeAtraccion tipoAtraccion, double duracion, double costo) {
+		this.nombre = nombre;
+		this.tipoAtraccion = tipoAtraccion;
+		this.duracion = duracion;
+		this.costo = costo;
+	}
+	
 	protected double getCosto() {
 		return this.costo;
 	}
