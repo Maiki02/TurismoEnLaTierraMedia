0905bb9465857a42400a9ad564a50465411e5196
MauHerman <mauherman@hotmail.com>
2021-08-30 21:34:23 -0300

    Implemento promocion y atraccion

--- src/producto/Atraccion.java
+++ src/producto/Atraccion.java
@@ -1,10 +1,48 @@
 package producto;
 
+<<<<<<< Updated upstream
 public class Atraccion {
 	
 	private String nombreAtraccion;
 
 	public Atraccion() {
+=======
+public class Atraccion extends Producto implements Comparable<Atraccion> {
 
+	private int cuposDisponibles;
+	public boolean ocuparAtraccion;
+	private int cupo;
+	
+	public Atraccion(String nombre, TipoDeAtraccion tipoAtraccion,
+			double duracion, double costo, int cupo) {
+	
+		
+		super(nombre,tipoAtraccion, duracion, costo  );
+		this.cupo = cupo;
+		this.cuposDisponibles = cupo;
 	}
+
+		
+	public boolean quedanCuposDisponibles() {
+		return cuposDisponibles > 0;
+	}
+
+	public boolean ocuparAtraccion() {
+		if (this.quedanCuposDisponibles()) {
+			cuposDisponibles--;
+			return true;
+		}
+        System.out.println("No tiene más cupos");
+		return false;
+	}
+
+	
+	
+	@Override
+	public int compareTo(Atraccion o) {
+>>>>>>> Stashed changes
+
+		return 0;
+	}
+
 }
--- src/producto/Promocion.java
+++ src/producto/Promocion.java
@@ -1,8 +1,52 @@
 package producto;
 
+<<<<<<< Updated upstream
 public class Promocion {
 	
 	public Promocion() {
+=======
+import java.util.ArrayList;
+import java.util.List;
+
+
+//Modificar monto por getCosto;
+public class Promocion extends Producto {
+
+	protected List<Atraccion> atracciones;
+
+	public Promocion(String nombre, TipoDeAtraccion tipoAtraccion, ArrayList<Atraccion> atracciones) {
+		super(nombre, tipoAtraccion, 0, 0);
+		this.atracciones = atracciones;
+	}
+
+	@Override
+	public double getCosto() {
+
+		double costo = 0;
+
+		for (Atraccion atraccion : atracciones) {
+			costo += atraccion.getCosto();
+
+		}
+		return costo;
+>>>>>>> Stashed changes
 
 	}
+
+	@Override
+	public double getDuracion() {
+
+		double duracion = 0;
+
+		for (Atraccion atraccion : atracciones) {
+			duracion += atraccion.getDuracion();
+
+		}
+		return duracion;
+	}
+
+	public double importeAPagar() {
+      return getCosto();
+	}
+
 }
